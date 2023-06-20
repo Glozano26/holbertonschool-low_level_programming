44ba4eb6d5705df6fc76cd9be7ec03f9d87a66af
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-20 12:01:08 -0700

    Patience, persistence

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit
+ * Return: (0)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}

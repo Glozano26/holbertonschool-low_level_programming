fb57696618f9a72b1edf984620d83deaecabe210
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-21 11:16:44 -0700

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+
+	return (0);
+}

bd11315e47d97f47e34f83c07fa1812955d82e1b
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-20 22:58:32 -0700

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: ALways Success(0)
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; ++i)
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+}

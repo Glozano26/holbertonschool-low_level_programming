81cbc948e87677c114866824ac649265b8abd0b1
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-21 17:49:44 -0700

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sig - function that prints the sign of a number
+ * Return: 1 and prints + n, 0 prints 0 and, -1 prints -
+ * @n: single letter input
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return ('+', 1);
+	}
+	else if (n == 0)
+	{
+		return (0, 0);
+	}
+	else if (n < 0)
+	{
+		return ('-', '/');
+	}
+}

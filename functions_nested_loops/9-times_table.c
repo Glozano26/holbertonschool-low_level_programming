3392b8a1d886db0b7a25f402327524ac1d03c1dd
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-22 14:08:51 -0700

    8-24_hours

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ *  main - Learn your times table -prints the 9 times table
+ *  Descripcion: prints the 9 times table, starting with 0.
+ *  Return: Always 0
+ */
+void times_table(void)
+{
+	int fila, columna, producto, cociente, residuo;
+
+	for (fila = 0; fila <= 9; fila++)
+	{
+		for (columna = 0; columna <= 9; columna++)
+		{
+			producto = fila * columna;
+			cociente = producto / 10;
+			residuo = producto % 10;
+
+			if (columna == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(residuo + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(cociente + '0');
+				_putchar(residuo + '0');
+			}
+		}
+		_putchar('\n');
+	}
+
+}

09fbdfd164cc0bbae0c89d054cfa525b2c9bbfbe
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-22 15:53:38 -0700

    add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,19 +2,16 @@
 #include <stdio.h>
 /**
  * add - adds two integers
- * @int - parameters
- * Return: Always 0.
+ * @a: integer 1
+ * @b: integer 2
+ * Return: return the result
  */
 int add(int a, int b)
 {
-	int sum = a + b;
+	int sum;
 
-	int tens = sum / 10;
+	sum = a + b;
 
-	int ones = sum % 10;
-
-	_putchar(tens + '0');
-	_putchar(ones + '0');
 
 	return (sum);
 }

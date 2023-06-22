b68912f5459cabba521fd0839b04cc795d6fc35a
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-21 17:58:17 -0700

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,14 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return ('+', 1);
+		printf("+");
+		return (1);
 	}
 	else if (n == 0)
 	{
-		return (0, 0);
+		printf("0");
+		return (0);
 	}
-	else if (n < 0)
+	else
 	{
-		return ('-', '/');
+		printf("-");
+		return (-1);
 	}
 }

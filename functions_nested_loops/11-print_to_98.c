f5dab7ac377a1dcee8d531dad205d445f2297c76
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-22 21:16:46 -0700

    print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,18 +8,33 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
-	{
-		printf("%d", n);
-	}
-	if (n < 98)
-	{
-		printf(", ");
-		print_to_98(n + 1);
-	}
-	else
-	{
+		if (n < 98)
+		{
+			while (n <= 98)
+			{
+				printf("%d", n);
+				if (n != 98)
+				{
+					printf(", ");
+				}
+				n++;
+			}
+		}
+		else if (n > 98)
+		{
+			while (n >= 98)
+			{
+				printf("%d", n);
+				if (n != 98)
+				{
+					printf(", ");
+				}
+				n--;
+			}
+		}
+		else
+		{
+			printf("98");
+		}
 		printf("\n");
-	}
-
 }

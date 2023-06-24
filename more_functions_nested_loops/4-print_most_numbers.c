0fd11934da3d090ed7e9989159d648d67ea99eb5
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-23 18:41:12 -0700

    prints

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0.
  */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	int i;
 
@@ -16,5 +16,4 @@ void print_most_numbers(void);
 		}
 	}
 	_putchar('\n');
-	return (0);
 }

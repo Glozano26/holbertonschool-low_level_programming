4dc6cb3531986420825d8eff5b8fb2852f49a300
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-28 10:08:51 -0700

    print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "mai.h"
+#include <stdio.h>
+/**
+ * print_rev - prints a string, in reverse
+ * @s: char input
+ * Return: Always 0
+ */
+void print_rev(char *s);
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+	{
+		i++;
+		i--;
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i];
+		i--;
+	}
+	_putchar('\n');
+}

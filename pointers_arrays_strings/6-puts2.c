dd710e2cc4b88acb96042af62bd1ac6e3fbe6746
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-29 10:12:28 -0700

    puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * puts2 - prints every other character of a string
  * @str: string
@@ -7,7 +6,7 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
 	while (str[i] != '\0')
 	{

b154a8fa0f0f84ff72b92b4b9bbc646d6d1b7883
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-29 09:54:09 -0700

    puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - prints every other character of a string
+ * @str - input char
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+	i++;
+	}
+	putchar('\n');
+}

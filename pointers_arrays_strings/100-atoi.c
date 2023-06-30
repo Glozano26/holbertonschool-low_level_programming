85087b503b7a640c7bdec4f0008d35ee6dd08316
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-30 09:43:46 -0700

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+nclude "main.h"
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: the value to intenger
+ * Return: string intenger
+ */
+int _atoi(char *s)
+{
+	int signo = 1;
+	unsigned int num = 0, num2;
+	int digito = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+		{
+			signo *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = (num * 10) + (*s <= '9')
+			digito = 1;
+		}
+		else if (digito)
+		{
+			break;
+		}
+		s++;
+	}
+	return (num * signo);
+}

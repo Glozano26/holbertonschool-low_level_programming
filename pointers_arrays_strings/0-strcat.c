8f71a8e047ca240153aabf70ed4331b800ce61df
Glozano26 <lozsuperbia2014@gmail.com>
2023-06-30 16:07:54 -0700

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,19 +8,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int len1 = 0, len2 = 0;
+	int len1 = 0;
 
-	while (dest[len1] != '\0')
+	while (*dest != '\0')
 	{
+		dest++;
 		len1++;
 	}
-	while (src[len2] != '\0')
+	while (*src)
 	{
-		dest[len1] = src[len2];
+		*dest = *src;
 		src++;
 		dest++;
+		len1++;
 	}
-	*dest = '\0';
+	*(dest + 1) = '\0';
+	dest = dest - len1;
 
 	return (dest);
 }

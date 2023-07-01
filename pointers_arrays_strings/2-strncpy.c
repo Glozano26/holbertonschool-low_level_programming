dd156824a6665dcd41932670ee36b12d9aeccc49
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-01 15:29:44 -0700

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: La cadena de destino
+ * @src: La cadena fuente
+ * @n: El número máximo de bytes a copiar de src
+ * Return: Puntero a la cadena de destino resultante
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{	
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);

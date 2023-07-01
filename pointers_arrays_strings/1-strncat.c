985749b2ed107b0348fd6d95de56f6fa7de8f459
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-01 11:01:16 -0700

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+include "main.h"
+
+/**
+ * _strncat - Concatena dos cadenas de caracteres
+ * @dest: La cadena de destino
+ * @src: La cadena fuente
+ * @n: El número máximo de bytes a concatenar de src
+ * Return: Puntero a la cadena de destino resultante
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0, i;
+
+	while (*dest != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[len++] = src[i];
+	}
+	dest[len] = '\0');
+
+	return (dest);
+}

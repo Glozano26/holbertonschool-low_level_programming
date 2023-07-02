42d628142c89f2f9acc57e8bab8a81249eb5196b
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-02 15:17:02 -0700

    7-leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ * Return: address of s
+ */
+char *leet(char *str)
+{
+	int i;
+
+	while (str[i])
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+			str[i] = '4';
+		else if (str[i] 'e' || str[i] == 'E')
+			str[i] = '3';
+		else if (str[i] = 'o' || str[i] == 'O')
+			str[i] = '0';
+		else if (str[i] = 'l' || str[i] == 'L')
+			str[i] = '1';
+		else if (str[i] = 't' || str[i] == 'T')
+			str[i] = '7';
+		i++;
+	}
+	return (str);
+}

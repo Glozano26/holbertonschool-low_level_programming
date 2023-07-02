ca9364bf0d21c885b199fb6bd602a7748622c9ab
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-02 15:20:31 -0700

    7-leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -13,13 +13,13 @@ char *leet(char *str)
 	{
 		if (str[i] == 'a' || str[i] == 'A')
 			str[i] = '4';
-		else if (str[i] 'e' || str[i] == 'E')
+		else if (str[i] =='e' || str[i] == 'E')
 			str[i] = '3';
-		else if (str[i] = 'o' || str[i] == 'O')
+		else if (str[i] == 'o' || str[i] == 'O')
 			str[i] = '0';
-		else if (str[i] = 'l' || str[i] == 'L')
+		else if (str[i] == 'l' || str[i] == 'L')
 			str[i] = '1';
-		else if (str[i] = 't' || str[i] == 'T')
+		else if (str[i] == 't' || str[i] == 'T')
 			str[i] = '7';
 		i++;
 	}

1cf45df70a09682759c346f31b89f8d19e64fdf4
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-06 15:54:34 -0700

    is_prime_number.c

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * is_prime_number_aux - variable auxiliar
+ * @n: integer
+ * @div: divisor
+ * Return: Always 0.
+ */
+int is_prime_number_aux(int n, int div)
+{
+	if (n <= 1)
+	{
+	return (0);
+	}
+	if (div * div > n)
+	{
+	return (1);
+	}
+	if (n % div == 0)
+	{
+	return (0);
+	}
+	return (is_prime_number_aux(n, div + 1));
+}
+/**
+ * is_primer_number - function
+ * @n: input
+ * Return: Always 0.
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_number_aux(n, 2));
+}

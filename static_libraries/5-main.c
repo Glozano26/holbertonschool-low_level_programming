dfb066d256aa1d9c2e5b44b4c26979a4d4820fd2
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-07 09:43:35 -0700

    create_static_lib.sh

--- static_libraries/5-main.c
+++ static_libraries/5-main.c
@@ -2,16 +2,9 @@ nclude "main.h"
 #include <stdio.h>
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
+ * main - check the code
+ * Return: Always 0.
+ */
+int main()
 {
-	    char s[10] = "My School";
-
-	        printf("%s\n", s);
-		    rev_string(s);
-		        printf("%s\n", s);
-			    return (0);
 }

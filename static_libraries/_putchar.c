ebab1eb6617c8926b8a6c68349550b9663b97a4e
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-07 09:18:36 -0700

    libmy.a, main.h

---
+++ static_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - Concatena dos cadenas de caracteres
+ * @dest: La cadena de destino
+ * @src: La cadena fuente
+ * @n: El número máximo de bytes a concatenar de src
+ * Return: Puntero a la cadena de destino resultante
+ */
+int _putchar(char c)
+{
+}

cab5a62730522a67155b08526bf40d338710200a
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-07 12:43:21 -0700

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: lenght string
+ * @argv: array
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+	{
+		printf("%s", argv[0]);
+	}
+	return (0);
+}
+

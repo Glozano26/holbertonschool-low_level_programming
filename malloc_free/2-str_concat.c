34d9937be2e0fd6d3a1fb1f4c3ab41fd38ef4bd9
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-11 20:57:02 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,11 +13,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	while (s1[i] != '\0')
+	while (s1 != NULL && s1[i] != '\0')
 	{
 		i++;
 	}
-	while (s2[c] != '\0')
+	while (s2 != NULL && s2[c] != '\0')
 	{
 		c++;
 	}

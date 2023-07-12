a933ed9aa731446ff0a5757ce0d1878f4490c5b7
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-11 19:59:38 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,66 +8,28 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, c, t, x;
+	int t, x, i = 0, c = 0;
 	char *conca;
-	i = 0;
-	c = 0;
 
-	if (s1 == NULL)
+	while (s1[i] != '\0')
 	{
-		if (s2 == NULL)
-		{
-			return (NULL);
-		}
-		while (s2[c] != '\0')
-		{
-			c++;
-
-			conca = malloc(sizeof(char) * (c + 1));
-			x = 0;
-			while (x <= c)
-			{
-				conca[x] = s2[x];
-				x++;
-			}
-			return (conca);
-		}
-		if (s2 == NULL)
-		{
-			while (s1[i] != '\0')
-			{
-				i++;
-			}
-			conca = malloc(sizeof(char) * (i + 1));
-			t = 0;
-			while (t <= i)
-			{
-				conca[t] = s1[t];
-				t++;
-			}
-			return (conca);
-		}
-		while (s1[i] != '\0')
-		{
-			i++;
-		}
-		while (s2[c] != '\0')
-		{
-			c++;
-		}
-		conca = malloc(sizeof(char) * (i + c + 1));
-		for (t = 0; t < i; t++)
-		{
-			conca[t] = s1[t];
-		}
-		x = 0;
-		while (t <= (i + c))
-		{
-			conca[t] = s2[x];
-			t++;
-			x++;
-		}
-	return (conca);
+		i++;
 	}
+	while (s2[c] != '\0')
+	{
+		c++;
+	}
+	conca = malloc(sizeof(char) * (i + c + 1));
+	for (t = 0; t < i; t++)
+	{
+		conca[t] = s1[t];
+	}
+	x = 0;
+	while (t <= (i + c))
+	{
+		conca[t] = s2[x];
+		t++;
+		x++;
+	}
+	return (conca);
 }
-

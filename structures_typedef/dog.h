2710ae6050824903eb38029a97c997471327da1a
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-17 10:48:00 -0700

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct_dog - new type
+* @name: first member
+* @age: second member
+* @owner: third member
+*/
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}new_dog;
+
+#endif 

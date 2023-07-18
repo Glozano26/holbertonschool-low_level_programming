604e4c9bd8064d496773c6e72cb3b39b2b7574b1
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-18 14:47:27 -0700

    2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include "function_pointers.h"
+
+/**
+ *int_index - funtion pointer
+ *@array: array.
+ *@size: size of the array
+ *@cmp: funtion pointed
+ *Return: nothing
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int c;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (c = 0; c < size; c++)
+	{
+		if (cmp(array[c]) != 0)
+		{
+			return (c);
+		}
+	}
+	return (-1);
+}

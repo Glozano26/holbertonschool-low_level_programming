ac29181d5e93f615d017d962da02a1cf01529092
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-20 15:34:37 -0700

    calc

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ *op_add - function
+ *@a: int
+ *@b: int
+ *Return: Result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - function
+ *@a: int
+ *@b: int
+ *Return: Result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - function
+ *@a: int
+ *@b: int
+ *Return: Result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - function
+ *@a: int
+ *@b: int
+ *Return: Result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - function
+ *@a: int
+ *@b: int
+ *Return: Result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

e396a621d9f3bf16fb938b3adae0233f2b914d71
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-21 13:29:15 -0700

    print num

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-	char *x = va_arg(num, char *s);
+		char *x = va_arg(num, char *);
 
 		if (x == NULL)
 		{

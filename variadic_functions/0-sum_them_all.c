e38e58bc630278bcdbfd30ae8ebe829f02578f2a
Glozano26 <lozsuperbia2014@gmail.com>
2023-07-26 21:28:42 -0700

    sum_them_all

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,14 +1,10 @@
 #include "variadic_functions.h"
-
-
 /**
  * sum_them_all - This function that returns the sum of all its parameters
  *
  * @n : arguments of array
  * Return: the sum or 0 if n = 0
  */
-
-
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;

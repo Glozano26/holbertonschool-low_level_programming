5d2f15c270c062e2bc7b406cf852952fe2d9c9b8
Glozano26 <lozsuperbia2014@gmail.com>
2023-08-02 17:04:45 -0700

    free

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - function that adds a new node at the end
+ * @head: pointer
+ * Return: the address of the new element
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+
+}

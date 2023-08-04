30e6d77c44078cee03a971da59bc718fbb390c36
Glozano26 <lozsuperbia2014@gmail.com>
2023-08-04 09:10:05 -0700

    node at the end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -29,7 +29,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		dlistint_t *lastNode;
 
 		lastNode = *head;
-		while (lastNode = lastNode->next)
+		while (lastNode->next != NULL)
 		{
 			lastNode = lastNode->next;
 		}

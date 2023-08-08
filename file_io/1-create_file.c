98e19bc9107be30ebff883d77a8534abaa376ffa
Glozano26 <lozsuperbia2014@gmail.com>
2023-08-08 15:34:53 -0700

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,6 +23,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		ssize_t bytes_write = write(fd, text_content, strlen(text_content));
+
 		if (bytes_write == -1)
 		{
 			close(fd);

19164ed1351e1b0daae50b8f49e24ecbd7867913
Glozano26 <lozsuperbia2014@gmail.com>
2023-08-11 14:56:20 -0700

    prueba

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -32,6 +32,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
+		hash_node_t *current = ht->array[index];
+
+		while (current->next != NULL)
+		{
+			if (strcmp(current->key, key) == 0)
+			{
+				free(current->value);
+				current->value = strdup(value);
+				return (1);
+			}
+			current = current->next;
+		}
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 		return (1);
